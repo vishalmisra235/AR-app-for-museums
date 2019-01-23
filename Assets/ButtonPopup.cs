2bf35aa54386cc5f2956d06e587652d7c2ffa279
vishalmisra235 <vishalmisra235@gmail.com>
2019-01-23 18:10:26 +0530

    adding files

---
+++ Assets/ButtonPopup.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+using UnityEngine.SceneManagement;
+
+public class ButtonPopup : MonoBehaviour, ITrackableEventHandler
+{
+
+    private TrackableBehaviour mTrackableBehaviour;
+
+    private bool mShowGUIButton = false;
+    private Rect mButtonRect = new Rect(50, 50, 400, 200); 
+
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if(mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED)
+        {
+            mShowGUIButton = true;
+        }
+        else
+        {
+            mShowGUIButton = false;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (mShowGUIButton)
+        {
+            // draw the GUI button
+            if (GUI.Button(mButtonRect, "Info"))
+            {
+                SceneManager.LoadScene("TextScene");
+
+
+                    // do something on button click 
+            }
+        }
+    }
+}
---
+++ Assets/Spartanback.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Spartanback : MonoBehaviour
+{
+
+
+    private bool mShowGUIButton = true;
+    private Rect mButtonRect = new Rect(40, 350, 200, 100);
+
+    void OnGUI()
+    {
+        if (mShowGUIButton)
+        {
+            // draw the GUI button
+            if (GUI.Button(mButtonRect, "Back"))
+            {
+                SceneManager.LoadScene("ar2");
+
+
+                // do something on button click 
+            }
+        }
+    }
+}
